92c48924f4a84d6e7a8ab48668638f9e455daccc
Syuugo <pub@s1204.me>
2025-05-17 23:34:58 +0900

    Initial commit

---
+++ app/src/main/java/jp/ed/shiga_ec/setatech_h/ccc/android/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ed.shiga_ec.setatech_h.ccc.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
